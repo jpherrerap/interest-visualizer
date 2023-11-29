aa7d2077b29a374c8f313c233e2da5dc59c95566
Juan Pablo Herrera <juherrera@ug.uchile.cl>
2023-11-29 20:43:02 -0300

    feat: working interest calculator + tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,98 @@
-import Image from 'next/image'
+"use client";
+import React, { useState } from 'react';
 import styles from './page.module.css'
 
-export default function Home() {
+const CompoundInterestCalculator = () => {
+  const [initial, setInitial] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [time, setTime] = useState(0);
+  const [compoundFrequency, setCompoundFrequency] = useState('1');
+  const [accumInterest, setAccumInterest] = useState(0);
+  const [compoundedInitial, setCompoundedInitial] = useState(0);
+  const [contribution, setContribution] = useState(0);
+  const [contributionTotalInterest, setContributionTotalInterest] = useState(0);
+  const [totalGain, setTotalGain] = useState(0);
+  const [finalAmount, setFinalAmount] = useState(0);
+
+  const calculateInterest = () => {
+    const p = parseFloat(initial);
+    const r = parseFloat(rate) / 100;
+    const t = parseFloat(time);
+    const n = parseFloat(compoundFrequency);
+    const c = parseFloat(contribution);
+  
+
+    const coeff = 1 + r / n;
+    const amount = p * Math.pow(coeff, n * t);
+    const interest = amount - p;
+    const ct = c * (coeff * (Math.pow(coeff, t) - 1)) / (r / n);
+    const totalContributions = c*t;
+    const contributionInterest = ct - c*t;
+
+
+    setAccumInterest(interest.toFixed(2));
+    setCompoundedInitial(amount.toFixed(2));
+    setContributionTotalInterest(contributionInterest.toFixed(2));
+    setTotalGain((interest + contributionInterest).toFixed(2));
+    setFinalAmount((amount + ct).toFixed(2));
+  };
+
   return (
     <main className={styles.main}>
+    <div className="p-2">        
+      <h1 className="text-2xl bottom-4">Interés Compuesto</h1>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <h2>Con esta herramienta podrás calcular el crecimiento de una inversión o ahorro. Puedes calcularlo en pesos tomando la tasa de interés nominal o en UF tomando la tasa de interés real.</h2>
+      </div>
+      <div className="p-2">
+      <div>
+        <label>
+          Depósito inicial: 
+          <input type="number" value={initial} className="field" onChange={(e) => setInitial(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Contribución anual: 
+          <input type="number" value={contribution} className="field" onChange={(e) => setContribution  (e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Tasa de interés anual (%): 
+          <input type="number" value={rate} className="field" onChange={(e) => setRate(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Tiempo (años): 
+          <input type="number" value={time} className="field" onChange={(e) => setTime(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Aplicar interés: 
+          <select value={compoundFrequency} className="block p-1.5 rounded-md" onChange={(e) => setCompoundFrequency(e.target.value)}>
+            <option value="1">Anualmente</option>
+            <option value="2">Semestralmente</option>
+            <option value="4">Trimestralmente</option>
+            <option value="12">Mensualmente</option>
+          </select>
+        </label>
+      </div>
+      </div>
+      <div>
+        <button className="p-2 bg-sky-500 hover:bg-sky-700 rounded-lg" onClick={calculateInterest}>Calcular interés</button>
+      </div>
+      {compoundedInitial !== null && (
+        <div className="p-2">
+          <h2 className="text-2xl">Capital acumulado: ${finalAmount}</h2>
+          <h2 className="text-2xl">Interés: ${totalGain}</h2>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
+    </div>
     </main>
-  )
-}
+  );
+};
+
+export default CompoundInterestCalculator;
